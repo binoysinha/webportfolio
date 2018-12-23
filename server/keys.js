6e061ceb75a99f245cd794a7c1d510dc97b6b2c8
Binoy Sinha <binoy.sinha@inmobi.com>
2018-12-24 00:31:41 +0530

    cleanup

--- server/keys.js
+++ server/keys.js
@@ -2,5 +2,5 @@ module.exports = {
     redisHost: process.env.REDIS_HOST,
     redisPort: process.env.REDIS_PORT,
     serverPort: process.env.SERVER_PORT,
-    sendGridKey: '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' || process.env.SENDGRID_API_KEY
+    sendGridKey: process.env.SENDGRID_API_KEY
 }
