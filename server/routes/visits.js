4e95b0921b67875c2faaf477d63945585329e488
Binoy Sinha <binoy.sinha@inmobi.com>
2018-12-25 19:13:50 +0530

    small css change

--- server/routes/visits.js
+++ server/routes/visits.js
@@ -7,7 +7,7 @@ const client = redis.createClient({
     retry_strategy: () => 1000
 });
 
-client.set('visits', 123);
+client.set('visits', 126);
 
 module.exports = app => {
     app.get('/visits', (req, res) => {
